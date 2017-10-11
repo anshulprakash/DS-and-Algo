5ec75ce0cbd9a275e0f36136b31320ad58a85ebf
anshulprakash <anshul.prakash17@gmail.com>
2017-10-10 21:17:06 -0500

    Day 19

--- Common Codes/ReverseList.java
+++ Common Codes/ReverseList.java
@@ -1,14 +1,22 @@
+
+
 public ListNode reverseList(ListNode head) {
     
-    ListNode prev = null;
-    
-    while(head != null){
-        ListNode front = head.next;
-        head.next = prev;
-        prev = head;
-        head = front;
+    if(head == null || head.next == null){
+        return head;
     }
+    ListNode prev = null, curr = head, next = head.next;
+    
+    while(next != null){
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+        next = next.next;
+    }
+    
+    curr.next = prev;
+    
+    return curr;
 
-    return prev;
 
 }
