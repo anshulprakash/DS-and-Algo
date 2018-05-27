b322ebf42bd19f5fa07aafc0de5edd7f6c0d4ee0
Anshul Prakash <anshul.prakash17@gmail.com>
2018-05-27 17:10:50 -0500

    Code Rust Day 5

---
+++ Code Rust/Math and Stats/findKthPermutation.java
@@ -0,0 +1,25 @@
+static int factorial(int n) {
+  if (n == 0 || n == 1) return 1;
+  return n * factorial(n -1 );
+}
+
+static void find_kth_permutation(
+    List<Character> v,
+    int k,
+    StringBuilder result) {
+
+  if (v.isEmpty()) {
+    return;
+  }
+
+  int n = v.size();
+  // count is number of permutations starting with first digit
+  int count = factorial(n - 1);
+  int selected = (k - 1) / count;
+
+  result.append(v.get(selected));
+  v.remove(selected);
+
+  k = k - (count * selected);
+  find_kth_permutation(v, k, result);
+}
