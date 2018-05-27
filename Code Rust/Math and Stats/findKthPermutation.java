5ae43ed4f469e605c7728ed38fabfea6d9234bb6
Anshul Prakash <anshul.prakash17@gmail.com>
2018-05-27 17:27:50 -0500

    Updated decription

--- Code Rust/Math and Stats/findKthPermutation.java
+++ Code Rust/Math and Stats/findKthPermutation.java
@@ -1,3 +1,4 @@
+//Divide two integers without using '/' (division) or '*' (multiplication) operators.
 static int factorial(int n) {
   if (n == 0 || n == 1) return 1;
   return n * factorial(n -1 );
